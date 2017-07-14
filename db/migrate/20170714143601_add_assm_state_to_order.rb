ade4cd9bcb2fe3ee1da060785a7fbabe99df65d0
sihhan <thehappyend95@gmail.com>
2017-07-14 23:04:11 +0800

    fix error at migration

--- db/migrate/20170714143601_add_assm_state_to_order.rb
+++
@@ -1,6 +0,0 @@
-class AddAssmStateToOrder < ActiveRecord::Migration[5.0]
-  def change
-    add_column :orders, :assm_state, :string, default: "order_placed"
-    add_index :orders, :assm_state
-  end
-end
